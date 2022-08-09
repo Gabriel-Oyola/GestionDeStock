ed29bbafa3dd6ae9019b6f762223694ebe341399
Gabriel-Oyola <90019281+Gabriel-Oyola@users.noreply.github.com>
2022-08-09 13:11:32 -0300

    Agregar archivos de proyecto.

---
+++ Shared/WeatherForecast.cs
@@ -0,0 +1,15 @@
+namespace GestionDeStock.Shared
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public string? Summary { get; set; }
+        /// <summary>
+        /// //
+        /// </summary>
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+}
